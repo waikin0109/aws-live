dc9b898c0acf631dc917b24f2ec7c938276489ab
Simon Wong <126314861+waikin0109@users.noreply.github.com>
2025-12-10 15:29:00 +0800

    Delete aws-live-main/aws-live-main directory

--- aws-live-main/aws-live-main/config.py
+++
@@ -1,7 +0,0 @@
-customhost = "database-1.c1izmb8tviah.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "bait3273"
-customdb = "employee"
-custombucket = "wongwaikin-employee"
-customregion = "us-east-1"
-
