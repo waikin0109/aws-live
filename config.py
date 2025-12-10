6950fc89ddb1069ba1444cb608e4df5c0c97aa30
Simon Wong <126314861+waikin0109@users.noreply.github.com>
2025-12-10 15:38:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "database-1.c1izmb8tviah.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "bait3273"
+custompass = "bmit3273"
 customdb = "employee"
 custombucket = "wongwaikin-employee"
 customregion = "us-east-1"
